c83b7dbd6b776aec7ba3d0f9d0a77d3f25ca3f5b
Kabita Bhurtel <kabitabhurtel222@gmail.com>
2026-07-22 19:49:38 +0545

    Fix component import path, nest Contact/About under Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./componets/Navbar.jsx";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,30 +6,39 @@ import CreatePost from "./pages/CreatePost";
 import PostDetail from "./pages/PostDetail";
 import Dashboard from "./pages/Dashboard";
 import EditPost from "./pages/EditPost";
+import Layout from "./components/Layout.jsx";
+import Contact from "./pages/Contact.jsx";
+import AboutUs from "./pages/Aboutus.jsx";
+import Blogs from "./pages/blogs.jsx";
 
 function App() {
   const token = localStorage.getItem("token");
 
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="posts/:id" element={<PostDetail />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route
+            path="create"
+            element={token ? <CreatePost /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="dashboard"
+            element={token ? <Dashboard /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="edit/:id"
+            element={token ? <EditPost /> : <Navigate to="/login" />}
+          />
+        </Route>
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route
-          path="/create"
-          element={token ? <CreatePost /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/edit/:id"
-          element={token ? <EditPost /> : <Navigate to="/login" />}
-        />
       </Routes>
     </BrowserRouter>
   );
